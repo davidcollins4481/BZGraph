93895f897bff86b72ea35399eae1ad58002429f6
David Collins <davidcollins4481@gmail.com>
2015-03-15 00:37:19 -0400

    syncing in UI

--- public/homeView/homeView.js
+++ public/homeView/homeView.js
@@ -6,21 +6,27 @@ homeViewController.controller('HomeCtrl',
     function($scope, $http) {
         
         $scope.bugsLastSynced = localStorage['bugsLastSynced'] || 'no data synced' ;
-        
-        $scope.sync = function($event) {
-            console.log('syncing');
 
-            console.log('clearing local storage');
+        $scope.loading = 'none';
+
+        $scope.sync = function($event) {
+            var button = $event.currentTarget;
+            button.disabled = true;
             localStorage['bugs'] = undefined;
+            $scope.loading = 'inline-block';
             
             $http.get('/sync')
                 .success(function(data, status, headers, config) {
                     localStorage['bugs'] = JSON.stringify(data);
                     localStorage['bugsLastSynced'] = new Date();
                     $scope.bugsLastSynced = localStorage['bugsLastSynced'];
+                    button.disabled = false;
+                    $scope.loading = 'none';
                 })
                 .error(function(data, status, headers, config) {
-
+                    alert("an error occurred")
+                    button.disabled = false;
+                    $scope.loading = 'none';
                 })
         }
     }
